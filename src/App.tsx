a09cde6d6a0cc7c5d895598bc8812ca246c35a19
Aliaksandr91 <horbachalex91@gmail.com>
2023-10-31 15:03:06 +0100

    feat: add fetch, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header} from "./components/Header";
 import {Categories} from "./components/Categories";
 import {Sort} from "./components/Sort";
 import {PizzaBlock} from "./components/PizzaBlock";
-import pizzas from './assets/pizzas.json'
 import './scss/app.scss'
 
-console.log(pizzas)
-
+type PizzaType = {
+    "id": number
+    "imageUrl": string
+    "title": string
+    "types": number[]
+    "sizes": number[]
+    "price": number
+    "category": number
+    "rating": number
+}
 
 function App() {
+    const [items, setItems] = useState([])
+    useEffect(() => {
+        fetch('https://6540fd8045bedb25bfc3032e.mockapi.io/items')
+            .then(res => res.json())
+            .then(items => setItems(items))
+    }, [])
     return (
         <div className="wrapper">
             <Header/>
@@ -21,7 +34,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza}/>)}
+                        {items.map((pizza: PizzaType) => <PizzaBlock key={pizza.id} {...pizza}/>)}
                     </div>
                 </div>
             </div>
