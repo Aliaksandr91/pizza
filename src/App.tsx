c745f7181279cf186385c9efc34e5087f3da9341
Aliaksandr91 <horbachalex91@gmail.com>
2023-11-09 19:43:44 +0100

    feat: add Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,25 @@ import {Header} from "./components/Header";
 import './scss/app.scss'
 import {Home} from "./pages/Home";
 import {Route, Routes} from "react-router-dom";
-import React, {useState} from "react";
+import React from "react";
 import {NotFound} from "./pages/NotFound";
 import {Cart} from "./pages/Cart";
 import {FullPizza} from "./pages/FullPizza";
-
-
-export type SearchContextType = {
-    searchValue: string;
-    setSearchValue: (value: string) => void;
-}
-// export const SearchContext = React.createContext();
+import {MainLayout} from "./layoouts/MainLayout";
 
 function App() {
     return (
-        <div className="wrapper">
 
-               <Header/>
-               <div className="content">
-                   <Routes>
-                       <Route path={'/'} element={<Home />}/>
-                       <Route path={'/cart'} element={<Cart/>}/>
-                       <Route path={'/pizza/:id'} element={<FullPizza/>}/>
-                       <Route path={'*'} element={<NotFound/>}/>
-                   </Routes>
-               </div>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route path={''} element={<Home/>}/>
+                <Route path={'cart'} element={<Cart/>}/>
+                <Route path={'pizza/:id'} element={<FullPizza/>}/>
+                <Route path={'*'} element={<NotFound/>}/>
+            </Route>
 
-        </div>
+        </Routes>
+
     );
 }
 
