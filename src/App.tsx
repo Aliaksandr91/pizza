fb784796373e8d2b0e50ab2691c8b5b8d29c2719
Aliaksandr91 <horbachalex91@gmail.com>
2023-11-13 20:09:57 +0100

    feat: add suspens lazy load Cart

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import {Home} from "./pages/Home";
 import {Route, Routes} from "react-router-dom";
 import React from "react";
 import {NotFound} from "./pages/NotFound";
-import {Cart} from "./pages/Cart";
+//import {Cart} from "./pages/Cart";
 import {FullPizza} from "./pages/FullPizza";
 import {MainLayout} from "./layoouts/MainLayout";
+
+const Cart = React.lazy(() => import("./pages/Cart")
+    .then(module => ({ default: module.Cart } as { default: React.ComponentType<any> })));
 
 function App() {
     return (
@@ -13,7 +16,11 @@ function App() {
         <Routes>
             <Route path={'/'} element={<MainLayout/>}>
                 <Route path={''} element={<Home/>}/>
-                <Route path={'cart'} element={<Cart/>}/>
+                <Route path={'cart'} element={
+                    <React.Suspense fallback={<>Loading...</>}>
+                        <Cart/>
+                    </React.Suspense>
+                }/>
                 <Route path={'pizza/:id'} element={<FullPizza/>}/>
                 <Route path={'*'} element={<NotFound/>}/>
             </Route>
